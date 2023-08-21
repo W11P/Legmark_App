23354511eb91879c48f398c4d296ee22aea18110
W11P <137043352+W11P@users.noreply.github.com>
2023-08-21 10:25:31 +0100

    Update Website Ranking.py

--- pages/Website Ranking.py
+++ pages/Website Ranking.py
@@ -23,16 +23,11 @@ if key_words != "":
             new_key_word +="+"
         else:
             new_key_word += space
-    #st.write(new_key_word)
 
-    #key_words = "SEO"
     target_url='https://www.google.co.uk/search?q='+new_key_word+'&num=100'
     st.write(target_url)
 
     response = requests.get(target_url, headers=headers)
-
-    # Response status code from Google
-    #print(response.status_code)
 
     # Parse response into Beautiful Soup
     soup = BeautifulSoup(response.text,'html.parser')
@@ -40,7 +35,7 @@ if key_words != "":
     # Extract all search results by looking up the first class
     results = soup.find_all("div", class_="MjjYud")
 
-    #st.write(results)
+   
     position = 0
     for result in range(0,len(results)):
 
@@ -48,28 +43,22 @@ if key_words != "":
         # Parse each url and look for the class yuRUbf to make get the correct URL
         try:
             web_site=results[result].find(class_='qLRx3b tjvcx GvPZzd cHaqb').text
-        #    st.write(result, web_site)
+            st.write(web_site)
         except:
-        #    st.title(result,"not here")
+     
             pass
 
         try:
             link= urlparse(results[result].find('a').get('href')).netloc
-            #st.write(link)
-            #st.write(len(link))
             if len(link)==0:
-               # st.write('pick it up')
                 pass
-        #except:
-        #    pass
-        #if link is not None or link != " " or link != "" or len(link)==0:
-            else:# len(link)!=0 or link is not None or link !="" or link !=" ":
-                st.write(len(link))
+            else:
+             
                 link = remove_www(link)
-                #print(link)
+                
                 site_position.append(link)
                 lenth.append(len(link))
-                #st.write(link)
+           
                 if(link == search_for_domain):
                     found = True
                     position = result
@@ -79,11 +68,11 @@ if key_words != "":
                     found = False
         except:
             pass
-    # We found the domain we are looking for
+    
     if(found == True):
         st.write("Found at position", position)
 
-    # We did not find the domain we are looking for
+   
     else:
         st.write("Not found in top", len(results))
 
